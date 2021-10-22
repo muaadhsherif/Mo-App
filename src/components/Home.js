9f398b20e9806b0c0004ed902e616d6be2cd3cb1
Muaadh Sherif <mu13abouhoraira@gmail.com>
2021-10-22 20:04:13 +0200

    Toggle between Movies & TVShows Components

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,45 @@
-function Home() {
-	return <div>Home</div>
+import { connect } from 'react-redux'
+import Movies from './Movies'
+import TVShows from './TVShows'
+
+function Home(props) {
+	const showUI = props.show === 'TVShows' ? <TVShows /> : <Movies />
+
+	return (
+		<>
+			<div className='toggle_show'>
+				<button
+					className={props.show !== 'TVShows' ? 'active ' : ''}
+					id='movies'
+					onClick={(e) => props.selectShowType(e.target.id)}
+				>
+					Movies
+				</button>
+				<button
+					className={props.show === 'TVShows' ? 'active ' : ''}
+					id='TVShows'
+					onClick={(e) => props.selectShowType(e.target.id)}
+				>
+					TV Shows
+				</button>
+			</div>
+			{showUI}
+		</>
+	)
 }
 
-export default Home
+const mapStateToProps = (state) => {
+	return {
+		show: state.show
+	}
+}
+
+const mapDispatchToProps = (dispatch) => ({
+	selectShowType: (show) =>
+		dispatch({
+			type: 'SELECT_SHOW_TYPE',
+			show
+		})
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home)
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,10 +1,10 @@
 const rootReducer = (state, action) => {
 	switch (action.type) {
-		case 'COUNT':
-			return { ...state, count: action.i }
-
 		case 'LOGIN':
 			return { ...state, userName: action.userName }
+
+		case 'SELECT_SHOW_TYPE':
+			return { ...state, show: action.show }
 
 		default:
 			return { ...state }
