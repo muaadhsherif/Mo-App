acb9f5ef7b7dd00522309ed3fcf6d2371efac355
muaadhsherif <mu13abouhoraira@gmail.com>
2021-10-28 20:37:52 +0200

    TopRatedMovies Component

--- src/components/Movies/sections/TopRatedMovies.js
+++ src/components/Movies/sections/TopRatedMovies.js
@@ -1,5 +1,53 @@
-function TopRated() {
-	return <div>TopRated</div>
+import { connect } from 'react-redux'
+import fetchMovies from '../../fetchMovies'
+
+function TopRated(props) {
+	const movies = props.movies
+	if (movies) {
+		return (
+			<table className='movies_table'>
+				<thead>
+					<tr>
+						<th />
+						<th>Title</th>
+						<th>Release Date</th>
+						<th>Vote Average</th>
+						<th>Vote Count</th>
+					</tr>
+				</thead>
+				<tbody>
+					{movies.map((mov, n) => (
+						<tr key={mov.id}>
+							<td>{n + 1}</td>
+							<td>{mov.title}</td>
+							<td>{mov.release_date}</td>
+							<td>{mov.vote_average}</td>
+							<td>{mov.vote_count}</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		)
+	}
+	else {
+		fetchMovies(props, 'movie/top_rated')
+
+		return <div>Loading</div>
+	}
 }
 
-export default TopRated
+const mapStateToProps = (state) => {
+	return {
+		movies: state.topRatedMovies
+	}
+}
+
+const mapDispatchToProps = (dispatch) => ({
+	moviesToProps: (movies) =>
+		dispatch({
+			type: 'GET_TOP_RATED_MOVIES',
+			movies
+		})
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(TopRated)
